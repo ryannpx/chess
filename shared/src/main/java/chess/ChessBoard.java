aa98ffef25b3b6bb721ab4b46f282879f843a8d8
Ryann Penrod <ryannpenrod@gmail.com>
2026-09-11 13:59:40 -0600

    creating the board and reset board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -10,6 +13,20 @@ public class ChessBoard {
 private ChessPiece[][] board = new ChessPiece[8][8];
     private ChessPosition position;
     private ChessPiece piece;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board) && Objects.equals(position, that.position) && Objects.equals(piece, that.piece);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.deepHashCode(board), position, piece);
+    }
 
     //ChessPiece[][] = new ChessPiece[8][8];
     public ChessBoard() {
@@ -50,7 +67,38 @@ private ChessPiece[][] board = new ChessPiece[8][8];
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+        //this is where i build the board
+        board = new ChessPiece[8][8];
 
+        ChessPiece.PieceType[] backRank = { //board setup along the bottom
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK,
+        };
+
+
+//        8  r n b q k b n r     black
+//        7  p p p p p p p p
+//        6  (empty)
+//        5  (empty)
+//        4  (empty)
+//        3  (empty)
+//        2  P P P P P P P P
+//        1  R N B Q K B N R     white
+//        1 2 3 4 5 6 7 8
+
+        for (int col = 1; col <=8; col++) { //the one and 2 are the two bottom rows, 7 and 8 and the top ones so black
+            addPiece(new ChessPosition(1,col),new ChessPiece(ChessGame.TeamColor.WHITE, backRank[col -1])); //places the white backrank
+            addPiece(new ChessPosition(2,col),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)); //puts white pawns on the next row
+            addPiece(new ChessPosition(8,col),new ChessPiece(ChessGame.TeamColor.BLACK, backRank[col -1]));
+            addPiece(new ChessPosition(7,col),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+
+        }
 
         //throw new RuntimeException("Not implemented");
     }
